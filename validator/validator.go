9de1729acb356925e87df2f880e3decf001d08f3
Jerry Wang <freebufer.wang@gmail.com>
2022-04-25 15:30:24 +0800

    Delete IsDir function

--- validator/validator.go
+++ validator/validator.go
@@ -7,9 +7,3 @@ func Exists(path string) bool {
 	_, err := os.Lstat(path)
 	return !os.IsNotExist(err)
 }
-
-// IsDir 判断所给路径是否是目录
-func IsDir(path string) bool {
-	stat, err := os.Lstat(path)
-	return stat.IsDir()
-}
