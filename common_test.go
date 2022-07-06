86c145868adc602a454ed581097a00c3749456e2
Jerry Wang <freebufer.wang@gmail.com>
2022-07-06 11:44:03 +0800

    style(common.go): Format 'common.go'.

--- common_test.go
+++ common_test.go
@@ -5,15 +5,15 @@ import (
 )
 
 func TestContains(t *testing.T) {
-        t.Log(Contains([]int{1,2,3}, 3))
+	t.Log(Contains([]int{1, 2, 3}, 3))
 
-        t.Log(Contains([]int{1,2,3}, 4))
+	t.Log(Contains([]int{1, 2, 3}, 4))
 
-        t.Log(Contains([]string{"aa","hello","bb"}, "bb"))
+	t.Log(Contains([]string{"aa", "hello", "bb"}, "bb"))
 
-        t.Log(Contains([]string{"aa","hello","bb"}, "ss"))
+	t.Log(Contains([]string{"aa", "hello", "bb"}, "ss"))
 
-        t.Log(Contains([]float64{33.22, 23.54, 7.8}, 7.8))
+	t.Log(Contains([]float64{33.22, 23.54, 7.8}, 7.8))
 
-        t.Log(Contains([]float64{33.22, 23.54, 7.8}, 7.9))
+	t.Log(Contains([]float64{33.22, 23.54, 7.8}, 7.9))
 }
