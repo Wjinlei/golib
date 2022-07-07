da3d38b85ac0ff9983953efa8a6f59636b5907d0
Jerry Wang <freebufer.wang@gmail.com>
2022-07-07 12:32:09 +0800

    feat(validator): Add HasString function.

--- validator/validator.go
+++ validator/validator.go
@@ -1,6 +1,9 @@
 package validator
 
-import "os"
+import (
+	"os"
+	"strings"
+)
 
 // Exists 判断所给路径文件/文件夹是否存在
 func Exists(path string) bool {
@@ -11,6 +14,16 @@ func Exists(path string) bool {
 func Has[T comparable](a []T, b T) bool {
 	for _, element := range a {
 		if element == b {
+			return true
+		}
+	}
+	return false
+}
+
+// Has TrimSpace for each element
+func HasString(a []string, b string) bool {
+	for _, element := range a {
+		if strings.TrimSpace(element) == b {
 			return true
 		}
 	}
